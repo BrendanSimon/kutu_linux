dc948cbf476ded56c561fc529d6a9305e568e1be
Greg Smart <Greg.Smart@kutu.com.au>
2015-07-09 17:51:08 +1000

    added custom build script

---
+++ kutu_build.sh
@@ -0,0 +1,7 @@
+source /opt/Xilinx/Vivado/2015.2/settings64.sh
+source /opt/Xilinx/SDK/2015.2/settings64.sh
+export CROSS_COMPILE=arm-xilinx-linux-gnueabi-
+export ARCH=arm
+make zynq_kutu_defconfig
+make uImage LOADADDR=0x00008000
+
