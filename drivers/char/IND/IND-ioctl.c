5976dc7924127fa69d75e456d5450886a03860dc
Greg Smart <greg.smart@kutu.com.au>
2015-12-23 13:52:50 +1100

    updated interrupt control

--- drivers/char/IND/IND-ioctl.c
+++ drivers/char/IND/IND-ioctl.c
@@ -49,9 +49,9 @@ int IND_Set_User_Mode(struct IND_drvdata *IND, struct IND_cmd_struct *cmd)
    }
 
    if (cmd->interrupt == ENABLE_INTERRUPT)
-       IND_write_reg(IND, R_INTERRUPT_ADDR, CLEAR_INTERRUPT);
+       IND_write_reg(IND, R_INTERRUPT_ADDR, K_CLEAR_INTERRUPT);
    else
-       IND_write_reg(IND, R_INTERRUPT_ADDR, DISABLE_INTERRUPT);
+       IND_write_reg(IND, R_INTERRUPT_ADDR, K_DISABLE_INTERRUPT);
 
 //   printk(KERN_DEBUG "IND_USER_SET_MODE: 0x%x 0x%x 0x%x 0x%x\n",cmd->config,cmd->address,cmd->capture_count,cmd->delay_count);
 
