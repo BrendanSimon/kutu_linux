16020e1ce55e873b6d28e4f1da3d6751aa1b9d1c
Greg Smart <greg.smart@kutu.com.au>
2017-09-07 15:08:49 +1000

    added LXS driver and build script

--- LXS_build.sh
+++ LXS_build.sh
@@ -2,6 +2,6 @@ source /opt/Xilinx/Vivado/2017.2/settings64.sh
 source /opt/Xilinx/SDK/2017.2/settings64.sh
 export CROSS_COMPILE=arm-xilinx-linux-gnueabi-
 export ARCH=arm
-make zynq_LSI_defconfig
+make zynq_LXS_defconfig
 make uImage LOADADDR=0x00008000
-./scripts/dtc/dtc -I dts -O dtb -o devicetree.dtb ./arch/arm/boot/dts/zynq-LSI.dts
+./scripts/dtc/dtc -I dts -O dtb -o devicetree.dtb ./arch/arm/boot/dts/zynq-LXS.dts
